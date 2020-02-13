c3688cf74e756c89ee1a717e5c26265ab3dc18a8
monireh <monireh.rasouli20@gmail.com>
2020-02-13 12:22:02 +0000

    the new version of the Health Tracker

---
+++ app/src/main/java/com/example/healthtracker/DatabaseHelper.kt
@@ -0,0 +1,9 @@
+package com.example.healthtracker
+
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper {
+}
+
+//create SQL table query in database
---
+++ app/src/main/java/com/example/healthtracker/User.kt
@@ -0,0 +1,6 @@
+package com.example.healthtracker
+
+import androidx.appcompat.app.AppCompatActivity
+
+class User: AppCompatActivity (){
+}
