f83b659622cc58d57df9c4c96188b5080e27dd20
monireh <monireh.rasouli20@gmail.com>
2020-03-02 10:42:02 +0000

    tried to implement the appointment activity, but not done yet

--- app/src/main/java/com/example/healthtracker/Appointments.kt
+++ app/src/main/java/com/example/healthtracker/Appointments.kt
@@ -1,7 +1,13 @@
 package com.example.healthtracker
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.appointments.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class Appointments : AppCompatActivity() {
 
@@ -11,6 +17,24 @@ class Appointments : AppCompatActivity() {
         setContentView(R.layout.appointments)
         databaseHelper = DatabaseHelper(this)
 
+
+        //calender
+        val calendarvalue=findViewById<View>(R.id.date_txt)
+        val pickdate= findViewById<View>(R.id.pickDate_btn)
+        val cal= Calendar.getInstance()
+
+        pickdate.setOnClickListener(View.OnClickListener {
+            val date = object:DatePickerDialog.OnDateSetListener{
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                    cal.set(Calendar.YEAR,year)
+                    cal.set(Calendar.MONTH,month)
+                    cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+                   // calendarvalue.text=SimpleDateFormat("MM/dd/yyyy",Locale.UK).format(cal.getTime())
+                }
+
+            }
+            DatePickerDialog(this,date,cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
+        })
 
     }
 }
