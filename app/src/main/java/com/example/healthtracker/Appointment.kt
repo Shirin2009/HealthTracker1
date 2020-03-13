0756d2f616015b782d9321044b013ec07c548ea7
Andreas Constantinou <aconstantinou3@sheffield.ac.uk>
2020-03-13 03:48:13 +0000

    Appointment class

---
+++ app/src/main/java/com/example/healthtracker/Appointment.kt
@@ -0,0 +1,3 @@
+package com.example.healthtracker
+
+class Appointment(var id: Int, var userID: Int, var description: String, var date: String, var time: String, var room: String, var doctorName: String, var selected: Int)
